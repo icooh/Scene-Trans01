55bdd383237c818cfe4511975c41cde5c0751531
icooh <icooh@naver.com>
2018-10-12 23:07:21 +0900

    Initial Commit

---
+++ Scene-Trans01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scene-Trans01
+//
+//  Created by icooh on 2018. 10. 12..
+//  Copyright © 2018년 myname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
